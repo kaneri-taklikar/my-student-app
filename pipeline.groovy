b02795564850bf1b5e369f110adcb5a3437f0408
kaneri <kaneritaklikar7@gmail.com>
2025-11-15 16:33:11 +0530

    pipeline added

---
+++ pipeline.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent {label,'slave'}
+    stages { 
+      stage( 'git checkout') {
+           steps {
+              git branch:'main', url: 'https://github.com/kaneri-taklikar/my-student-app.git'
+          }
+       } 
+      stage( 'build-stage') { 
+          steps {
+              sh '/opt/maven/bin/mbn clean package'
+          }
+      }
+      stage( 'test-stage') {
+           steps { 
+               echo "this is test-stage"
+           }
+      }
+      stage( 'deploy-stage') {
+           steps {
+              echo "this is deply-stage"
+           } 
+       }
+    } 
+ }
+     
