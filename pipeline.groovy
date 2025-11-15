dbd0351ab3ba7f3c147334e6d3eec45232c5b8fd
kaneri <kaneritaklikar7@gmail.com>
2025-11-15 16:55:45 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,26 +1,22 @@
 pipeline {
     agent { label 'slave' }
-    stages { 
-      stage( 'git checkout') {
-           steps {
-              git branch:'main', url: 'https://github.com/kaneri-taklikar/my-student-app.git'
-          }
-       } 
-      stage( 'build-stage') { 
-          steps {
-              sh '/opt/maven/bin/mbn clean package'
-          }
-      }
-      stage( 'test-stage') {
-           steps { 
-               echo "this is test-stage"
-           }
-      }
-      stage( 'deploy-stage') {
-           steps {
-              echo "this is deply-stage"
-           } 
-       }
-    } 
- }
+    stages {
+        stage('Git Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/kaneri-taklikar/my-student-app.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh '/opt/maven/bin/mbn clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "Running tests..."
+            }
+        }
+    }
+}
+
      
